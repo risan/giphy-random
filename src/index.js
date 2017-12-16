bdfd45e0c104fc62602fe0d7f09ec2167dc489aa
risan <risanbagja@gmail.com>
2017-12-16 03:24:22 +0100

    :sparkles: GiphyRandom class

---
+++ src/index.js
@@ -0,0 +1,40 @@
+import axios from 'axios';
+
+export default class GiphyRandom {
+  static get URI() {
+    return 'https://api.giphy.com/v1/gifs/random';
+  }
+
+  constructor({apiKey, defaultRating = 'G', uri = GiphyRandom.URI} = {}) {
+    if (!apiKey) {
+      throw new Error('The apiKey parameter is required.');
+    }
+
+    if (!axios) {
+      throw new Error('The giphy-random package requires axios library.');
+    }
+
+    this.apiKey = apiKey;
+    this.defaultRating = defaultRating;
+    this.uri = uri;
+  }
+
+  get({ tag, rating = this.defaultRating } = {}) {
+    const params = tag ? { api_key: this.apiKey, tag, rating } : { api_key: this.apiKey, rating };
+
+    return new Promise((resolve, reject) => {
+      axios.get(this.uri, { params }).then(response => {
+        resolve(response.data.data);
+      }).catch(error => {
+        if (error.response) {
+          const { msg, status } = error.response.data.meta;
+          reject(new Error(`Failed requesting random GIF from Giphy: [${status}] ${msg}`));
+        } else if (error.request) {
+          reject(new Error('Failed requesting random GIF from Giphy, no response was received.'));
+        } else {
+          reject(error);
+        }
+      });
+    });
+  }
+}
