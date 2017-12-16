df096fe94cc912527717ab293cd799b8f72da4e8
risan <risanbagja@gmail.com>
2017-12-16 15:56:36 +0100

    :wrench: Move coverage setting to jest config file

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,7 @@
 module.exports = {
   verbose: true,
   testEnvironment: "node",
-  testMatch: ["**/tests/**/*.js"]
+  testMatch: ["**/tests/**/*.js"],
+  collectCoverage: true,
+  collectCoverageFrom: ["**/src/**/*.js"]
 };
