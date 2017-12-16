b9fbb0bc1d16a96a1fb5edb05848208209f9da62
risan <risanbagja@gmail.com>
2017-12-16 11:18:05 +0100

    :art: Apply prettier

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
-import axios from 'axios';
+import axios from "axios";
 
 export default class GiphyRandom {
   static get URI() {
-    return 'https://api.giphy.com/v1/gifs/random';
+    return "https://api.giphy.com/v1/gifs/random";
   }
 
-  constructor({ apiKey, defaultRating = 'G', uri = GiphyRandom.URI } = {}) {
+  constructor({ apiKey, defaultRating = "G", uri = GiphyRandom.URI } = {}) {
     if (!apiKey) {
-      throw new Error('The apiKey parameter is required.');
+      throw new Error("The apiKey parameter is required.");
     }
 
     if (!axios) {
-      throw new Error('The giphy-random package requires axios library.');
+      throw new Error("The giphy-random package requires axios library.");
     }
 
     this.apiKey = apiKey;
@@ -20,21 +20,34 @@ export default class GiphyRandom {
   }
 
   get({ tag, rating = this.defaultRating } = {}) {
-    const params = tag ? { api_key: this.apiKey, tag, rating } : { api_key: this.apiKey, rating };
+    const params = tag
+      ? { api_key: this.apiKey, tag, rating }
+      : { api_key: this.apiKey, rating };
 
     return new Promise((resolve, reject) => {
-      axios.get(this.uri, { params }).then((response) => {
-        resolve(response.data.data);
-      }).catch((error) => {
-        if (error.response) {
-          const { msg, status } = error.response.data.meta;
-          reject(new Error(`Failed requesting random GIF from Giphy: [${status}] ${msg}`));
-        } else if (error.request) {
-          reject(new Error('Failed requesting random GIF from Giphy, no response was received.'));
-        } else {
-          reject(error);
-        }
-      });
+      axios
+        .get(this.uri, { params })
+        .then(response => {
+          resolve(response.data.data);
+        })
+        .catch(error => {
+          if (error.response) {
+            const { msg, status } = error.response.data.meta;
+            reject(
+              new Error(
+                `Failed requesting random GIF from Giphy: [${status}] ${msg}`
+              )
+            );
+          } else if (error.request) {
+            reject(
+              new Error(
+                "Failed requesting random GIF from Giphy, no response was received."
+              )
+            );
+          } else {
+            reject(error);
+          }
+        });
     });
   }
 }
