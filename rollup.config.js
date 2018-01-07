90122cf3a84ecb1a30c69843d81c3f0b19192592
risan <risanbagja@gmail.com>
2018-01-07 06:33:09 +0100

    :art: Use single quote

--- rollup.config.js
+++ rollup.config.js
@@ -1,31 +1,31 @@
-import pkg from "./package.json";
-import babel from "rollup-plugin-babel";
-import resolve from "rollup-plugin-node-resolve";
-import commonjs from "rollup-plugin-commonjs";
-import eslint from "rollup-plugin-eslint";
+import pkg from './package.json';
+import babel from 'rollup-plugin-babel';
+import resolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
+import eslint from 'rollup-plugin-eslint';
 import uglify from 'rollup-plugin-uglify';
 
 const umdConfig = ({ minify = false } = {}) => {
   const config = {
-    input: "src/index.js",
+    input: 'src/index.js',
     output: {
       file: minify ? `dist/${pkg.name}.umd.min.js` : `dist/${pkg.name}.umd.js`,
-      format: "umd"
+      format: 'umd'
     },
-    name: "GiphyRandom",
-    external: ["axios"],
+    name: 'GiphyRandom',
+    external: ['axios'],
     globals: {
-      axios: "axios"
+      axios: 'axios'
     },
     plugins: [
       eslint({
-        include: ["src/**"],
+        include: ['src/**'],
         throwOnError: true,
         throwOnWarning: true
       }),
       resolve(),
       commonjs(),
-      babel({ exclude: "node_modules/**" })
+      babel({ exclude: 'node_modules/**' })
     ]
   };
 
@@ -40,25 +40,25 @@ export default [
   umdConfig(),
   umdConfig({ minify: true }),
   {
-    input: "src/index.js",
+    input: 'src/index.js',
     output: [
       {
         file: pkg.main,
-        format: "cjs"
+        format: 'cjs'
       },
       {
         file: pkg.module,
-        format: "es"
+        format: 'es'
       }
     ],
-    external: ["axios"],
+    external: ['axios'],
     plugins: [
       eslint({
-        include: ["src/**"],
+        include: ['src/**'],
         throwOnError: true,
         throwOnWarning: true
       }),
-      babel({ exclude: "node_modules/**" })
+      babel({ exclude: 'node_modules/**' })
     ]
   }
 ];
